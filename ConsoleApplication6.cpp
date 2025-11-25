ae9501cb37f39baf1f241b429d927bfb2bb5877b
Bartosz-Kudlinski <kbartosz380@gmail.com>
2025-11-25 12:41:28 +0100

    Update ConsoleApplication6.cpp

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -17,17 +17,18 @@ int main() {
 */
 
 //zad2
-int tab[10];
+/*int tab[10];
 int rozmiar = 10;
 
-int usun() {
+void usun() {
 	int pozycja;
 	cout << "Podaj pozycje od 0 do 9: ";
 	cin >> pozycja;
 	for (int i = pozycja; i < rozmiar - 1; i++) {
-		tab[i] = tab[i - 1];
+		tab[i] = tab[i + 1];
 	}
 	rozmiar--;
+
 }
 
 int main() {
@@ -37,8 +38,57 @@ int main() {
 	}
 
 	usun();
+
 	cout << "Tablica po usunieciu wartosci: ";
 	for (int i = 0; i < rozmiar; i++) {
 		cout << tab[i];
 	}
+	return 0;
+}
+*/
+
+//zad3
+float TEMP1[10];
+float TEMP2[10];
+int INDEKS = 0;
+
+float przelicz(float tempC) {
+    return tempC + 273.15f;
+}
+
+void zapisz(float tempPodana, float tempPrzeliczona) {
+    if (INDEKS < 10) {
+        TEMP1[INDEKS] = tempPodana;
+        TEMP2[INDEKS] = tempPrzeliczona;
+        INDEKS++;
+    }
+    else {
+        cout << "Koniec miejsca w tablicy, przeliczenie nie zostanie zapisane" << endl;
+    }
+}
+
+void wyswietl() {
+    cout << "Zapisane temperatury:" << endl;
+    for (int i = 0; i < INDEKS; i++) {
+        cout << i << ": " << TEMP1[i] << " C -> " << TEMP2[i] << " K" << endl;
+    }
+}
+
+int main() {
+    int ile;
+    cout << "Ile temperatur chcesz podac (max 10)? ";
+    cin >> ile;
+
+    for (int i = 0; i < ile; i++) {
+        float tC;
+        cout << "Podaj temperature w C: ";
+        cin >> tC;
+
+        float tK = przelicz(tC);
+        zapisz(tC, tK);
+    }
+
+    wyswietl();
+
+    return 0;
 }
